01e88bcac7bb9fcf2e7a376eacce1b2efc1791f9
leisza <leiszam@gmail.com>
2021-10-19 23:51:16 -0500

    Banco de datos del Back End

--- authApp/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .profile import Profile
-from .user import User
-from .product import Product
-from .exchange import Exchange
-from .favorite import Favorite
--- authApp/serializers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .profileSerializer import ProfileSerializer
-from .userSerializer import UserSerializer
-from .productSerializer import ProductSerializer
-from .exchangeSerizalizer import ExchangeSerializer
-from .favoriteSerizalizer import FavoriteSerializer
